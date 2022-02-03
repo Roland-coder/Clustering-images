8c8a3816a43fddf4d4d45916e0412bac27e7480e
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2022-02-03 11:12:13 +0200

    Update image_cluster.py

--- image_cluster.py
+++ image_cluster.py
@@ -9,3 +9,8 @@ st.write("This web app shows clusters of different amazon products based on desc
 
 df = pd.read_csv('clustered_description.csv')
 st.dataframe(df[['product_name','description']])
+grouped = df.groupby('cluster')
+
+for name, group in grouped:
+  print("Below are the products and description in cluster " + name)
+  print(group['description'])
