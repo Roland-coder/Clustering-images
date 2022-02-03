11aa54bf52138eb8c7d816d9413d661d4d20fc2e
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2022-02-03 11:01:04 +0200

    Create image_cluster.py

---
+++ image_cluster.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("Image Cluster App")
+st.header("Clustering Images Of E Commerce Products")
+st.write("This web app shows clusters of different amazon products based on description of the different product images")
+
+
+df = pd.read_csv('clustered_description.csv')
+st.dataframe(df)
